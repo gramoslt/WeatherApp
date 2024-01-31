0ba305187e9363be596326a0df50b235a3d2d8e1
Gerardo Ramos <gerx_37@hotmail.com>
2024-01-31 13:38:44 -0600

    Deleted ContentView and created WeatherView file

---
+++ WeatherApp/WeatherApp/Views/WeatherView/WeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherView.swift
+//  WeatherApp
+//
+//  Created by Gerardo Ramos on 31/01/24.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
