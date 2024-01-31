7cab42073e81de3c2085850c71e6d7fbfe93888a
Gerardo Ramos <gerx_37@hotmail.com>
2024-01-31 13:23:30 -0600

    Created WeatherApp XCode project

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Gerardo Ramos on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
